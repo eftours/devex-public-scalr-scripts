80ef58fe27c93bfdabda84cc18cdd359add9924b
Jeff Langevin <11415084+jlangevin@users.noreply.github.com>
2023-02-06 12:25:02 -0500

    add flag info, stop display of response data in scalr run console

--- trigger_scalr_workspace_run.sh
+++ trigger_scalr_workspace_run.sh
@@ -3,8 +3,12 @@
 DOWNSTREAM_WORKSPACE_ID=${1}
 
 # See: https://docs.scalr.com/en/latest/api/preview/runs.html#create-a-run
+# --slient           - Use silent mode / don't display progress
+# --show-error       - Show error even when silent mode is used
+# --output /dev/null - Redirect "info" to null / Don't display response info
+# --location         - follow redirects
 trigger_run() {
-  curl -X POST --silent --location \
+  curl -X POST --silent --show-error --output /dev/null --location \
     --url "https://${SCALR_HOSTNAME}/api/iacp/v3/runs" \
     -H "Content-Type: application/vnd.api+json" \
     -H "Authorization: Bearer ${SCALR_TOKEN}" \
